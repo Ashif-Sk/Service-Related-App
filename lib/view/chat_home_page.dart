3b3f9f8403e4be1225bbded03c6c39992fd861ab
Ashif-Sk <skashifm45687@gmail.com>
2025-06-26 12:36:17 +0530

    integrated image api

--- lib/view/chat_home_page.dart
+++ lib/view/chat_home_page.dart
@@ -5,7 +5,7 @@ import 'package:contrador/view/chat_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flexify/flexify.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../models/chat_room_model.dart';
 
 class ChatHomePage extends StatefulWidget {
@@ -28,22 +28,30 @@ class _ChatHomePageState extends State<ChatHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: _uiComponents.headline2("Messages",Theme.of(context).colorScheme.tertiary),
+        title: _uiComponents.headline2(
+            "Messages", Theme.of(context).colorScheme.tertiary),
       ),
       body: FutureBuilder<List<ChatRoomModel>>(
         future: _chatServices.getUserChatRooms(_userId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: _uiComponents.loading()
-            );
+            return Center(child: _uiComponents.loading());
           }
           if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text("No chats yet"));
+            return Center(
+              child: Image.asset(
+                'images/empty-chat.png',
+                fit: BoxFit.fill,
+                height: height * 0.25,
+                width: width * 0.5,
+              ),
+            );
           }
 
           var chatRooms = snapshot.data!;
@@ -54,7 +62,10 @@ class _ChatHomePageState extends State<ChatHomePage> {
               var chatRoom = chatRooms[index];
               var otherUserId = chatRoom.users
                   .firstWhere((id) => id != _userId); // Get the other user's ID
-
+              final now = DateTime.now();
+              final isToday = now.year == chatRoom.timeStamp.year &&
+                  now.month == chatRoom.timeStamp.month &&
+                  now.day == chatRoom.timeStamp.day;
               return FutureBuilder<DocumentSnapshot>(
                 future: FirebaseFirestore.instance
                     .collection('users')
@@ -68,33 +79,46 @@ class _ChatHomePageState extends State<ChatHomePage> {
                   String name = userData['name'] ?? "Unknown";
                   String profilePic = userData['imagePath'] ?? "";
 
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: ListTile(shape: const Border(bottom: BorderSide(color:Colors.grey,width: 0.5)),
-                      tileColor: Theme.of(context).colorScheme.primaryContainer,
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: profilePic.isNotEmpty
-                            ? NetworkImage(profilePic)
-                            : const AssetImage('images/profile.png')
-                                as ImageProvider,
-                      ),
-                      title: _uiComponents.headline3(name),
-                      subtitle: Text(chatRoom.lastMessage,
-                          maxLines: 1, overflow: TextOverflow.ellipsis),
-                      trailing: Text(
-                        "${chatRoom.timeStamp.day}/${chatRoom.timeStamp.month}/${chatRoom.timeStamp.year}", // Format time
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                      onTap: () {
-                        Flexify.go(
-                            ChatPage(
-                                receiverId: otherUserId,
-                                chatRoomId: chatRoom.chatRoomId),
-                            animation: FlexifyRouteAnimations.slide,
-                            animationDuration: const Duration(milliseconds: 400));
-                      },
+                  return ListTile(
+                    shape: const Border(
+                        bottom: BorderSide(color: Colors.grey, width: 0.5)),
+                    // tileColor: Theme.of(context).colorScheme.primaryContainer,
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: profilePic.isNotEmpty
+                          ? NetworkImage(profilePic)
+                          : const AssetImage('images/profile.png')
+                              as ImageProvider,
                     ),
+                    title: _uiComponents.headline3(name),
+                    subtitle: Text(
+                      chatRoom.lastMessage,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.abel(
+                          textStyle: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey.shade900)),
+                    ),
+                    trailing: Text(
+                      isToday
+                          ? "Today"
+                          : "${chatRoom.timeStamp.day}/${chatRoom.timeStamp.month}/${chatRoom.timeStamp.year}", // Format time
+                      style: GoogleFonts.abel(
+                          textStyle: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey.shade900)),
+                    ),
+                    onTap: () {
+                      Flexify.go(
+                          ChatPage(
+                              receiverId: otherUserId,
+                              chatRoomId: chatRoom.chatRoomId),
+                          animation: FlexifyRouteAnimations.slide,
+                          animationDuration: const Duration(milliseconds: 400));
+                    },
                   );
                 },
               );
